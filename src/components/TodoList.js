3ba3a41ca55a5b6b4fae4e35ea1748e86fe6d86e
nithinbairu <nithinbairu08@gmail.com>
2025-10-28 16:59:43 +0530

    1st commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function TodoList(props) {
-    const { todos, handleComplete } = props;
+function TodoList({ todos, handleComplete }) {
   return (
     <div>
-        <h3>Child Component</h3>
-        <ul>
-          {todos.map(todo => (
-            <li key={todo.id}>
-              {todo.title}
-              {todo.completed ? "": (
-                <button onClick={() => handleComplete(todo.id)}>Complete</button>
-              )}
-            </li>
-          ))}
-        </ul>
+      <h3>Child Component</h3>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            {" "}
+            {todo.completed ? (
+              <span style={{ color: "green" }}>✔️ Completed</span>
+            ) : (
+              <button onClick={() => handleComplete(todo.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
